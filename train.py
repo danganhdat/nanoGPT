57e545f2a61ea7186f943f4a393dc589b82894c6
danganhdat3004 <danganhdat3004@gmail.com>
2024-10-17 23:44:09 +0000

    batch size 16

--- train.py
+++ train.py
@@ -47,7 +47,7 @@ wandb_project = 'owt'
 wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
 dataset = 'vndataset'
-gradient_accumulation_steps = 4 * 8 # used to simulate larger batch sizes
+gradient_accumulation_steps = 4 * 8 # x batch_size must = 512? # used to simulate larger batch sizes
 batch_size = 16 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 1024
 # model
@@ -58,7 +58,7 @@ dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
 bias = False # do we use bias inside LayerNorm and Linear layers?
 # adamw optimizer
 learning_rate = 6e-4 # max learning rate
-max_iters = 18_890 # 600000 # total number of training iterations
+max_iters = 18_890 # 9_903_287_246/2**19 & batch size = 16 # 600000 # total number of training iterations
 weight_decay = 1e-1
 beta1 = 0.9
 beta2 = 0.95
